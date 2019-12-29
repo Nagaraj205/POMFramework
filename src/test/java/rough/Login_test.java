5272ee73052d114439105358dd81276fc5834b3d
nagaraj <nagaraj.g0122@gmail.com>
2019-12-29 10:03:02 +0530

    login status Change

--- src/test/java/rough/Login_test.java
+++ src/test/java/rough/Login_test.java
@@ -12,15 +12,15 @@ public class Login_test
 	
 	public static void main(String[] agrs) throws InterruptedException 
 	{
-		
+		//This is a Rough Package
 		Homepage home = new Homepage();
 		LoginPage login = home.goToLogin();
 		Zoho_Apppage zoho = login.doLogin("nagaraj.g.work@gmail.com", "Zoho@123#");
 		zoho.gotoCRM();
-//		Acc_page acc = Page.menu.gotoAccountsTab();
-//		Create_Acc_Page create = acc.gotoCreate_acc();
-//		create.create_acc("Nagaraj G");
-//		Thread.sleep(2000);
+		Acc_page acc = Page.menu.gotoAccountsTab();
+		Create_Acc_Page create = acc.gotoCreate_acc();
+		create.create_acc("Nagaraj G");
+		Thread.sleep(2000);
 		
 		Page.driver.quit();
 		
